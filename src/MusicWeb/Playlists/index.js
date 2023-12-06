dcc218cb0c5f88c465707fa805e7784f20d880a3
Xuyang Wang <we24e@cangshuqiaodaimademac.local>
2023-12-06 16:36:07 -0500

    added deleting tracks from playlist and styling

--- src/MusicWeb/Playlists/index.js
+++ src/MusicWeb/Playlists/index.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import * as client from './client';
 import { fetchItemDetails } from '../Search/util';
 import { AccessTokenContext } from '../AccessTokenContext';
+import * as userClient from '../users/client';
+import { ListGroup, ListGroupItem, Button, Alert, Card } from 'react-bootstrap';
 
 function Playlists() {
     const { playlistId } = useParams();
@@ -41,41 +43,67 @@ function Playlists() {
         fetchPlaylistDetails();
     }, [playlistId]);
 
+    const [profile, setProfile] = useState(null);
+    const fetchProfile = async () => {
+        try {
+            const profileData = await userClient.profile();
+            setProfile(profileData);
+        } catch (error) {
+            console.error("Error fetching profile:", error);
+        }
+    };
+    useEffect(() => {
+        fetchProfile();
+    }, []);
+    const handleDeleteTrack = async (trackId) => {
+        if (window.confirm("Are you sure you want to delete this track?")) {
+            await client.deleteTrackFromPlaylist(playlistId, trackId);
+            fetchPlaylistDetails();
+        }
+    };
+
     return (
-        <div>
-            <h2>Playlist Details</h2>
+        <div className="container mt-4">
             {playlist ? (
-                <div>
-                    <h3>{playlist.title}</h3>
-                    <ul>
+                <Card>
+                    <Card.Header as="h2">PlayList: {playlist.title}</Card.Header>
+                    <ListGroup variant="flush">
                         {tracksDetails.map((track, index) => (
-                            <li key={track.id || index}>
-                                <h4>{track.name}</h4>
-                                <p>Artist: {track.artists.map(artist => artist.name).join(', ')}</p>
-                                <p>Album: {track.album.name}</p>
-                                <p>Duration: {formatDuration(track.duration_ms)}</p>
-                                {track.preview_url && (
-                                    <audio controls src={track.preview_url}>
-                                        Your browser does not support the audio element.
-                                    </audio>
-                                )}
-                                <p>
-                                    <a href={track.external_urls.spotify} target="_blank" rel="noopener noreferrer">
-                                        Listen on Spotify
-                                    </a>
-                                </p>
-                                {track.album.images && track.album.images.length > 0 && (
-                                    <img src={track.album.images[0].url} alt={track.album.name} />
-                                )}
-                            </li> 
+                            <ListGroupItem key={track.id || index}>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <h4>{track.name}</h4>
+                                        <p>Artist: {track.artists.map(artist => artist.name).join(', ')}</p>
+                                        <p>Album: {track.album.name}</p>
+                                        <p>Duration: {formatDuration(track.duration_ms)}</p>
+                                        {track.preview_url && (
+                                            <audio controls src={track.preview_url}>
+                                                Your browser does not support the audio element.
+                                            </audio>
+                                        )}
+                                        <p>
+                                            <a href={track.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+                                                Listen on Spotify
+                                            </a>
+                                        </p>
+                                    </div>
+                                    <div>
+                                        {track.album.images && track.album.images.length > 0 && (
+                                            <img src={track.album.images[0].url} alt={track.album.name} style={{ width: '150px', height: 'auto' }} />
+                                        )}
+                                        {profile && profile._id === playlist.userId && (
+                                            <Button variant="danger" onClick={() => handleDeleteTrack(track.id)}>Delete</Button>
+                                        )}
+                                    </div>
+                                </div>
+                            </ListGroupItem> 
                         ))}
-                    </ul>
-                </div>
+                    </ListGroup>
+                </Card>
             ) : (
-                <p>Loading playlist...</p>
+                <Alert variant="info">Loading playlist...</Alert>
             )}
         </div>
     );
 }
-
 export default Playlists;
